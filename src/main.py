a910283aa4910588df84d4243fb0a64a5963b4ea
Karthik <karthikk1162@gmail.com>
2026-04-17 23:12:51 +0530

    Run command function defined which runs a shell command using subprocess

--- src/main.py
+++ src/main.py
@@ -1,2 +1,18 @@
+##imports
 import sys
 import subprocess
+
+def run_command(command: str):
+    """
+    Runs a shell command using subprocess
+    """
+    try:
+        result = subprocess.run(
+            command,
+            shell=True,
+            check=True
+        )
+        return result.returncode
+    except subprocess.CalledProcessError as e:
+        print(f"Error: Command failed with exit code {e.returncode}")
+        sys.exit(e.returncode)
