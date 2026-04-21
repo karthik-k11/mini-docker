7947aed22a87b9256d58d39a94af99b010eb156e
Karthik <karthikk1162@gmail.com>
2026-04-21 11:22:51 +0530

    Subprocess finished,  process creation- fork() and exec()

--- src/main.py
+++ src/main.py
@@ -1,21 +1,39 @@
-##imports
+##Imports
 import sys
-import subprocess
+import os
+
 
 def run_command(command: str):
-    
-    try:
-        result = subprocess.run(
-            command,
-            shell=True,
-            check=True
-        )
-        return result.returncode
-    except subprocess.CalledProcessError as e:
-        print(f"Error: Command failed with exit code {e.returncode}")
-        sys.exit(e.returncode)
 
-##Main function()
+    print(f"Running command: {command}")
+
+    ##fork process
+    pid = os.fork()
+
+    if pid == 0:
+        ##CHILD PROCESS
+        try:
+            ##Split command into list
+            args = command.split()
+
+            ##Replace current process with new program
+            os.execvp(args[0], args)
+
+        except Exception as e:
+            print(f"Execution failed: {e}")
+            sys.exit(1)
+
+    else:
+        ##PARENT PROCESS
+        _, status = os.waitpid(pid, 0)
+
+        exit_code = os.WEXITSTATUS(status)
+
+        if exit_code != 0:
+            print(f"Error: Command failed with exit code {exit_code}")
+            sys.exit(exit_code)
+
+##Main function
 def main():
     if len(sys.argv) < 3:
         print("Usage: python main.py run <command>")
@@ -25,7 +43,6 @@ def main():
 
     if action == "run":
         command = sys.argv[2]
-        print(f"Running command: {command}")
         run_command(command)
     else:
         print(f"Unknown command: {action}")
